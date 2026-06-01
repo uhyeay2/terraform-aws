3769fd22cdd021341b08965fbc76f3b036c46b43
Daniel Aguirre <uhyeay24@gmail.com>
2026-06-01 10:23:11 -0500

    add policy for aws_kms_key

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -42,6 +42,21 @@ resource "aws_kms_key" "cloudwatch_logs" {
   deletion_window_in_days = 7
   enable_key_rotation     = true
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "Enable IAM User Permissions"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action    = "kms:*"
+        Resource  = "*"
+      }
+    ]
+  })
+
   tags = merge(
     {
       Name        = "${local.name_prefix}-cloudwatch-kms"
